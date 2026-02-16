5423e674ef384c8662b695895c8cbae481700d90
Suresh021 <ssc760951@gmail.com>
2026-02-16 11:12:27 +0530

    Updated file structure

---
+++ file35.js
@@ -0,0 +1,14 @@
+//const names = ["Surya", "Ajay", "Vijay", "Suresh", "Ramesh"];
+const names = new Array("Surya", "Ajay", "Vijay", "Suresh", "Ramesh");
+// names.push("Naresh");
+// names.pop()
+//names.shift()
+//names.unshift("Naresh");
+//shift and unshift is slower because of rearrangement
+// console.log(names);
+// console.log(names.sort())
+// console.log(names.reverse())
+
+const numbers = [15, 2, 1, 8, 3]
+console.log(
+    numbers.sort(() => a - b));
